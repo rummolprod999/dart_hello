84d62bae604105ed9b73df883001eaf7a4d70927
rummolprod999 <rummolprod999@gmail.com>
2019-12-04 15:52:56 +0300

    upd

--- bin/main.dart
+++ bin/main.dart
@@ -2,4 +2,7 @@ import 'package:dart_hello/dart_hello.dart' as dart_hello;
 
 main(List<String> arguments) {
   print('Hello world: ${dart_hello.calculate()}!');
+  int a1 = 2;
+  int b1 = 5;
+  print(a1&b1);
 }
